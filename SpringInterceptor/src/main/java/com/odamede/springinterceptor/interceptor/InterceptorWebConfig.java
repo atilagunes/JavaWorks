c22fc472160fe307012b297ed1ace589aafec02e
Mevlüt Atila Güneş <atilagunes@yaani.com>
2024-04-09 11:55:22 +0300

    logging fix

--- SpringInterceptor/src/main/java/com/odamede/springinterceptor/interceptor/InterceptorWebConfig.java
+++ SpringInterceptor/src/main/java/com/odamede/springinterceptor/interceptor/InterceptorWebConfig.java
@@ -1,14 +1,15 @@
 package com.odamede.springinterceptor.interceptor;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class InterceptorWebConfig implements WebMvcConfigurer {
 
-/*    @Override
+    @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // Add LoggingInterceptor and CompressionInterceptor to interceptors registry
         registry.addInterceptor(new LoggingInterceptor()).addPathPatterns("/**");
-    }*/
+    }
 }
